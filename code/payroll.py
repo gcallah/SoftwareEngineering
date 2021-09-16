169a82d2fa1619fcd01ec0fbb2622c2d37be14b0
Gene Callahan <gcallah@mac.com>
2021-09-16 13:35:03 -0400

    Adding payroll example.

---
+++ code/payroll.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+
+
+def calc_pay(emp, rate, hours):
+    return 0
+
+
+def main():
+    emp = "Callahan"
+    net_pay = calc_pay(emp, 14.50, 30)
+    print(f"Pay {emp} ${net_pay}")
+
+
+if __name__ == "__main__":
+    main()
+
